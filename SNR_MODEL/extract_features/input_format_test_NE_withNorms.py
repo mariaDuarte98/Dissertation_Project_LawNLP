47ff6244f648dc6cb2f5d0664aaa0927b47ac09e
mariaDuarte98 <78866601+mariaDuarte98@users.noreply.github.com>
2021-09-14 19:15:50 +0100

    Add files via upload

--- SNR_MODEL/extract_features/input_format_test_NE_withNorms.py
+++ SNR_MODEL/extract_features/input_format_test_NE_withNorms.py
@@ -1,22 +1,27 @@
-#run with python3
+# Run with python3
 
 import json
 import os
-ner_labels_first_level = 'DEF,OBLIG,RIGHT,LEFFECT,INTRO,IGNORE'
 
-punctuation = [".",":","!","?",";",","]
 
 less100 = ["IGNORE", "REF", "URL", "SITUATION", "RESULT", "NE_","NE_ORG", "RATIONALE",  "NE_OFFICE", "NE_PERSON","TIME_", "TIME_DATE_ABS", "TIME_DATE_REL_ENUNC", "TIME_FREQ", "DEF-EXCLUSION", "DEF-EXEMPLUM", "AGENT", "PATIENT" ]
 
 
 NE_labels = ["LREF","TREF","NE_ADM","TIME_DATE_REL_TEXT","TIME_DURATION"]
 
+punctuation = [".",":","!","?",";",","]
 
-new_corpus = open('testAfterRules_26_07_2021_cor_READY.jsonl','r')
+
+new_corpus = open('testAfterRules_26_07_2021_cor_READY.jsonl','r') #this files correspond to the annotations of the test set
 lines = new_corpus.readlines()
 sentences = [json.loads(jline) for jline in lines]
-#sentences = sentences_ents
 new_corpus.close()
+
+test_ners = open("../Norms_pred_test_ners.jsonl","r")  # This file that contains norms model predictions to be added into a segment
+lines = test_ners.read()
+true_ners = json.loads(lines)["ners"] # Get predicted NORMS
+test_ners.close()
+
 final_ners = []
 real_final_sents = []
 final_ids = []
@@ -26,19 +31,19 @@ for sentence in sentences:
     sent = sentence["sentence"]
     final_ids.append(sentence["id"])
 
+    # The following for is responsible for adding NE  gold labels into a segment, so that when adding the Norms predicted labels, a new mapping of the NE gold spans indexes is done (regarding the segment with the norms information)
     for ner in ners_sent:
         if ner[2] not in less100 and ner[2] in NE_labels:
             TAG += 1
             sent[ner[0]] += ' ' + str(TAG) + 'I__' +  ner[2]
             sent[ner[1]] += ' ' + str(TAG) + 'F__' +  ner[2]
 
-    test_ners = open("../Norms_pred_test_ners.jsonl","r")
-    lines = test_ners.read()
-    true_ners = json.loads(lines)["ners"]
+
+    id_ners = []
     if sentence["id"] in true_ners.keys():
         id_ners = true_ners[sentence["id"]]
-    else:
-        id_ners = []
+    
+    # The following for is responsible for adding NORM predicted labels into a segment
     for ner in id_ners:
         if ner[2] in ['DEF','OBLIG','RIGHT','LEFFECT','INTRO']:
             if ner[2] == 'OBLIG':
@@ -50,6 +55,7 @@ for sentence in sentences:
             sent[ner[0]] = 'I' + ner[2] + ' ' + sent[ner[0]]
             sent[ner[1]] += ' F' + ner[2]
 
+    # Saving final segment tokens. Example: ['IOBRIG' , '2', '-', 'Organização', '1I__NE_ADM', ... 'mundial', '1F__NE_ADM', ..., 'FOBRIG']
     fin_sent = []
     for token in sent:
         fin_sent += token.split(' ')
@@ -58,28 +64,20 @@ for sentence in sentences:
     token_index = -1
     final_sent = []
     for token in fin_sent:
-        if 'I__' in token or 'F__' in token:
-            token_label = token.split('__')[1]
-            if token_label not in labels_ners.keys() and 'I__' in token:
-                labels_ners[token.replace('I__','')] = []
-            if 'I__' in token:
-                labels_ners[token.replace('I__','')].append([token_index,'-',token_label])
-            if 'F__' in token:
-                not_found = True
-                il = - 1
-                while not_found:
-                    if labels_ners[token.replace('F__','')][il][1] == '-':
-                        labels_ners[token.replace('F__','')][il][1] = token_index
-                        not_found = False
-                    else:
-                        il -= 1
-        elif token != '' and token != ' ':
+        if 'I__' in token or 'F__' in token: # if token is NE label
+            token_label = token.split('__')[1] # get label from "ILABEL"/"FLABEL" string
+            if 'I__' in token: # if token is NE start label, save label start  position
+                labels_ners[token.replace('I__','')] = [token_index,'-',token_label]
+            if 'F__' in token: # if token is NE end label, save label end position
+                labels_ners[token.replace('F__','')][1] = token_index
+        elif token != '' and token != ' ': #if normal token (not NE label), update token position and add token to final sent
             final_sent.append(token)
             token_index += 1
+            
     sent_n = []
     for key in labels_ners.keys():
-        if labels_ners[key] != []:
-            sent_n += labels_ners[key]
+        sent_n.append(labels_ners[key])
+        
     final_ners.append(sent_n)
     real_final_sents.append(final_sent)
 
--- SNR_MODEL/extract_features/input_format_test_SR_withNorms.py
+++ SNR_MODEL/extract_features/input_format_test_SR_withNorms.py
@@ -1,23 +1,24 @@
-#run with python3
+# Run with python3
 
 import json
 import os
-
-ner_labels_first_level = 'DEF,OBLIG,RIGHT,LEFFECT,INTRO,IGNORE'
 
 labels = ["DEFINIENDUM","DEFINIENS","DEF-INCLUSION","DEF-EXCLUSION","DEF-EXEMPLUM","SCOPE","ACTION","CONDITION","CONCESSION","PURPOSE","EXPERIENCER","AGENT","PATIENT","OBJECT","THEME","EXCEPTION","EFFECT","RATIONALE","NEG"] #SRs
 
 less100 = ["IGNORE", "REF", "URL", "SITUATION", "RESULT", "NE_","NE_ORG", "RATIONALE",  "NE_OFFICE", "NE_PERSON","TIME_", "TIME_DATE_ABS", "TIME_DATE_REL_ENUNC", "TIME_FREQ", "DEF-EXCLUSION", "DEF-EXEMPLUM", "AGENT", "PATIENT", "OBJECT" ]
 
 
-
-
 punctuation = [".",":","!","?",";",","]
 
-new_corpus = open('testAfterRules_26_07_2021_cor_READY.jsonl','r') #this files correspond to test set of corpus
+new_corpus = open('testAfterRules_26_07_2021_cor_READY.jsonl','r') #this files correspond to the annotations of the test set
 lines = new_corpus.readlines()
 sentences = [json.loads(jline) for jline in lines]
 new_corpus.close()
+
+test_ners = open("../Norms_pred_test_ners.jsonl","r")  # This file that contains norms model predictions to be added into a segment
+lines = test_ners.read()
+true_ners = json.loads(lines)["ners"]  # Get predicted NORMS
+test_ners.close()
 
 final_ners = []
 real_final_sents = []
@@ -28,18 +29,18 @@ for sentence in sentences:
     sent = sentence["sentence"]
     final_ids.append(sentence["id"])
 
+    # The following for is responsible for adding SR  gold labels into a segment, so that when adding the Norms predicted labels, a new mapping of the  SR gold spans indexes is done (regarding the segment with the norms information)
     for ner in ners_sent:
-        if ner[2] not in less100 and ner[2] in labels:
+        if ner[2] not in less100 and ner[2] in labels: #adding SR gold labels into sentence
             TAG += 1
             sent[ner[0]] += ' ' + str(TAG) + 'I__' +  ner[2]
             sent[ner[1]] += ' ' + str(TAG) + 'F__' +  ner[2]
-    test_ners = open("../Norms_pred_test_ners.jsonl","r")
-    lines = test_ners.read()
-    true_ners = json.loads(lines)["ners"]
+            
+    id_ners = []
     if sentence["id"] in true_ners.keys():
         id_ners = true_ners[sentence["id"]]
-    else:
-        id_ners = []
+        
+    # The following for is responsible for adding NORM predicted labels into a segment
     for ner in id_ners:
         if ner[2] in ['DEF','OBLIG','RIGHT','LEFFECT','INTRO']:
             if ner[2] == 'OBLIG':
@@ -52,7 +53,7 @@ for sentence in sentences:
             sent[ner[1]] += ' F' + ner[2]
 
 
-                 
+    # Saving final segment tokens. Example: ['IOBRIG' , '2', '-', 'Publica', '1I__ACTION', ... 'fim', '1F__ACTION', ..., 'FOBRIG']
     fin_sent = []
     for token in sent:
         fin_sent += token.split(' ')
@@ -61,28 +62,20 @@ for sentence in sentences:
     token_index = -1
     final_sent = []
     for token in fin_sent:
-        if 'I__' in token or 'F__' in token:
-            token_label = token.split('__')[1]
-            if token_label not in labels_ners.keys() and 'I__' in token:
-                labels_ners[token.replace('I__','')] = []
-            if 'I__' in token:
-                labels_ners[token.replace('I__','')].append([token_index,'-',token_label])
-            if 'F__' in token:
-                not_found = True
-                il = - 1
-                while not_found:
-                    if labels_ners[token.replace('F__','')][il][1] == '-':
-                        labels_ners[token.replace('F__','')][il][1] = token_index
-                        not_found = False
-                    else:
-                        il -= 1
-        elif token != '' and token != ' ':
+        if 'I__' in token or 'F__' in token: # if token is SR label
+            token_label = token.split('__')[1]  # get label from "ILABEL"/"FLABEL" string
+            if 'I__' in token: # if token is SR start label, save label start  position
+                labels_ners[token.replace('I__','')] = [token_index,'-',token_label]
+            if 'F__' in token: # if token is SR end label, save label end position
+                labels_ners[token.replace('F__','')][1] = token_index
+        elif token != '' and token != ' ': #if normal token (not SR label), update token position and add token to final sent
             final_sent.append(token)
             token_index += 1
+            
     sent_n = []
     for key in labels_ners.keys():
-        if labels_ners[key] != []:
-            sent_n += labels_ners[key]
+        sent_n.append(labels_ners[key])
+        
     final_ners.append(sent_n)
     real_final_sents.append(final_sent)
 
@@ -91,6 +84,7 @@ if os.path.exists("testAfterRules_26_07_2021_cor_READY_READY_SR.jsonl"):
 ex = open('testAfterRules_26_07_2021_cor_READY_READY_SR.jsonl','w')
 print('testAfterRules_26_07_2021_cor_READY_READY_SR')
 print(len(final_ids))
+
 for index in range(0,len(final_ids)):
     json.dump({"id": final_ids[index], "ners": final_ners[index],  "sentence": real_final_sents[index]}, ex)
     ex.write("\n")
